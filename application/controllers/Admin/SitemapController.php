3454c8715557bcdd2acf71a7c97e1028cc26ae07
Milos Stefanovic <stefanovic.milos.srb@gmail.com>
2016-08-08 09:59:53 +0200

    Uradili smo dodavanje sitemap stranice.

--- application/controllers/Admin/SitemapController.php
+++ application/controllers/Admin/SitemapController.php
@@ -23,14 +23,16 @@ class Admin_SitemapController extends Zend_Controller_Action
         
         $cmsSitemapDbTable = new Application_Model_DbTable_CmsSitemapPages();
         
+        if($id != 0) {
+               
         $sitemapPage = $cmsSitemapDbTable->getSitemapPageById($id);
         
-        if(!$sitemapPage && $id != 0) {
+        if(!$sitemapPage) {
             throw new Zend_Controller_Router_Exception('No sitemap pages is found', 404);
         }
         
-        
-        
+        }
+     
         $childSitemapPages = $cmsSitemapDbTable->search(array(
             'filters' => array(
                 'parent_id' => $id
@@ -47,6 +49,236 @@ class Admin_SitemapController extends Zend_Controller_Action
         $this->view->sitemapPageBreadcrumbs = $sitemapPageBradcrumbs;
         $this->view->childSitemapPages = $childSitemapPages;
         $this->view->systemMessages = $systemMessages;
+        $this->view->currentSitemapPageId = $id;
     }
     
+    public function addAction() {
+         
+        $request = $this->getRequest();
+        
+        $parentId = (int) $request->getParam('parent_id', 0);
+        
+        if($parentId < 0){
+            throw new Zend_Controller_Router_Exception('Invalid id for sitemap pages.', 404);
+        }
+        
+        
+        $cmsSitemapPagesDbTable = new Application_Model_DbTable_CmsSitemapPages();
+        
+        if($parentId != 0) {
+            // Check if parent page exists
+            $parentSitemapPage = $cmsSitemapPagesDbTable->getSitemapPageById($parentId);
+            
+            if(!$parentSitemapPage) {
+            throw new Zend_Controller_Router_Exception('No sitemap page is found for id: ' . $parentId, 404);
+        }
+        }
+        
+        
+        
+        $flashMessenger = $this->getHelper('FlashMessenger');
+        
+          $systemMessages = array(
+            'success' => $flashMessenger->getMessages('success'),
+            'errors' => $flashMessenger->getMessages('errors')
+        );
+
+        $form = new Application_Form_Admin_SitemapPageAdd($parentId);
+
+        //default form data
+        $form->populate(array(
+            
+        ));
+
+      
+
+        if ($request->isPost() && $request->getPost('task') === 'save') {
+
+            try {
+
+                //check form is valid
+                if (!$form->isValid($request->getPost())) {
+                    throw new Application_Model_Exception_InvalidInput('Invalid data was sent for new Sitemap Page.');
+                }
+
+                //get form data
+                $formData = $form->getValues();
+                
+                // Set parent id for new page 
+                $formData['parent_id'] = $parentId;
+                
+                // remove key sitemap_page_photo from data because there is no column 'sitemap_page_photo' in cms_sitemapPages table
+                //unset($formData['sitemap_page_photo']);
+                // do actual task
+                //save to database etc
+             
+                // insert sitemapPage returns ID of the new sitemapPage
+                $sitemapPageId = $cmsSitemapPagesDbTable->insertSitemapPage($formData);
+                
+                
+                
+//                if($form->getElement('sitemap_page_photo')->isUploaded()){
+//                
+//                    // photo is uploaded
+//                    
+//                    $fileInfos = $form->getElement('sitemap_page_photo')->getFileInfo('sitemap_page_photo');
+//                    $fileInfo = $fileInfos['sitemap_page_photo'];
+//                    // $fileInfo = $_FILES['sitemapPage_phpto'];
+//                    
+//                    try{
+//                        // Open uploaded photo in temporary directory
+//                        $sitemapPagePhoto = Intervention\Image\ImageManagerStatic::make($fileInfo['tmp_name']);
+//                        
+//                        $sitemapPagePhoto->fit(150, 150);
+//                        
+//                        $sitemapPagePhoto->save(PUBLIC_PATH . '/uploads/sitemapPages/' . $sitemapPageId . '.jpg');
+//                        
+//                        
+//                        
+//                    } catch (Exception $ex) {
+//                        $flashMessenger->addMessage('SitemapPager has been saved, but error ocured during image processing', 'errors');
+//
+//                        //redirect to same or another page
+//                        $redirector = $this->getHelper('Redirector');
+//                        $redirector->setExit(true)
+//                                ->gotoRoute(array(
+//                                    'controller' => 'admin_sitemap',
+//                                    'action' => 'edit',
+//                                    'id' => $sitemapPageId
+//                                        ), 'default', true);
+//                    }
+//                    
+//                } 
+//                
+                //set system message
+                $flashMessenger->addMessage('Sitemap Page has been saved', 'success');
+
+                //redirect to same or another page
+                $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_sitemap',
+                            'action' => 'index',
+                            'id' => $parentId
+                                ), 'default', true);
+            } catch (Application_Model_Exception_InvalidInput $ex) {
+                $systemMessages['errors'][] = $ex->getMessage();
+            }
+        }
+        
+        $sitemapPageBradcrumbs = $cmsSitemapPagesDbTable->getSitemapPageBreadcrumbs($parentId);
+        
+        $this->view->sitemapPageBreadcrumbs = $sitemapPageBradcrumbs;
+        $this->view->parentId = $parentId;
+        $this->view->systemMessages = $systemMessages;
+        $this->view->form = $form;
+    }
+    
+    public function editAction() {
+        
+        $request = $this->getRequest();
+        
+        $id = (int) $request->getParam('id');
+        
+        if($id <= 0){
+            
+            // Prekida se izvrsavanje programa i prikazuje se page not found
+            throw  new Zend_Controller_Router_Exception('Invalid sitemapPage id: ' . $id, 404);
+        }
+        
+        $cmsSitemapPagesTable = new Application_Model_DbTable_CmsSitemapPages();
+        
+        $sitemapPage = $cmsSitemapPagesTable->getSitemapPageById($id);
+        
+        if(empty($sitemapPage)){
+            throw new Zend_Controller_Router_Exception('No sitemap page is found with id: ' . $id, 404);
+        }
+        
+        $flashMessenger = $this->getHelper('FlashMessenger');
+        
+        $systemMessages = array(
+            'success' => $flashMessenger->getMessages('success'),
+            'errors' => $flashMessenger->getMessages('errors')
+        );
+
+        $form = new Application_Form_Admin_SitemapPageEdit($sitemapPage['id'], $sitemapPage['parent_id']);
+
+        //default form data
+        $form->populate($sitemapPage);
+
+      
+
+        if ($request->isPost() && $request->getPost('task') === 'update') {
+
+            try {
+
+                //check form is valid
+                if (!$form->isValid($request->getPost())) {
+                    throw new Application_Model_Exception_InvalidInput('Invalid data was sent for sitemapPage.');
+                }
+
+                //get form data
+                $formData = $form->getValues();
+                
+                // do actual task
+                //save to database etc
+             
+                 // Update postojeceg zapisa u tabeli
+                
+//                unset($formData['sitemapPage_photo']);
+//                
+//                if($form->getElement('sitemapPage_photo')->isUploaded()){
+//                
+//                    // photo is uploaded
+//                    
+//                    $fileInfos = $form->getElement('sitemapPage_photo')->getFileInfo('sitemapPage_photo');
+//                    $fileInfo = $fileInfos['sitemapPage_photo'];
+//                    // $fileInfo = $_FILES['sitemapPage_phpto'];
+//                    
+//                    try{
+//                        // Open uploaded photo in temporary directory
+//                        $sitemapPagePhoto = Intervention\Image\ImageManagerStatic::make($fileInfo['tmp_name']);
+//                        
+//                        $sitemapPagePhoto->fit(150, 150);
+//                        
+//                        $sitemapPagePhoto->save(PUBLIC_PATH . '/uploads/sitemapPages/' . $sitemapPage['id'] . '.jpg');
+//                        
+//                        //$membecl->clean(Zend_Cache::CLEANING_MODE_ALL);
+//                        
+//                    } catch (Exception $ex) {
+//                        
+//                        throw new Application_Model_Exception_InvalidInput('Error ocured during image processing');
+//                        
+//                    }
+//                    
+//                }
+//                
+                $cmsSitemapPagesTable->updateSitemapPageById($sitemapPage['id'], $formData);
+                
+                
+                //set system message
+                $flashMessenger->addMessage('Sitemap Page has been updated', 'success');
+
+                //redirect to same or another page
+                $redirector = $this->getHelper('Redirector');
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_sitemap',
+                            'action' => 'index',
+                            'id' => $sitemapPage['parent_id']
+                                ), 'default', true);
+            } catch (Application_Model_Exception_InvalidInput $ex) {
+                $systemMessages['errors'][] = $ex->getMessage();
+            }
+        }
+
+        $sitemapPageBradcrumbs = $cmsSitemapPagesTable->getSitemapPageBreadcrumbs($sitemapPage['parent_id']);
+        
+        $this->view->sitemapPageBreadcrumbs = $sitemapPageBradcrumbs;
+        
+        $this->view->systemMessages = $systemMessages;
+        $this->view->form = $form;
+        $this->view->sitemapPage = $sitemapPage;
+        
+    }
 }
---
+++ application/forms/Admin/SitemapPageAdd.php
@@ -0,0 +1,75 @@
+<?php
+
+class Application_Form_Admin_SitemapPageAdd extends Zend_Form
+{
+    protected $parentID;
+    
+    public function __construct($parentId, $options = null) {
+        
+        $this->parentID = $parentId;
+        
+        parent::__construct($options);
+    }
+
+    
+    public function init() {
+        //type
+        // url_slug
+        // short_title
+        // title
+        //description 
+        //body
+        
+//        Zend_Form_Element_Select;
+//        Zend_Form_Element_Multiselect;
+//        Zend_Form_Element_MultiCheckbox;
+        
+        $type = new Zend_Form_Element_Select('type');
+        $type->addMultiOption('', '-- Select Sitemap Page Type --')
+                ->addMultiOptions(array(
+                            'StaticPage' => 'Static Page',
+        ))->setRequired(true);
+        
+        $this->addElement($type);
+        
+        $urlSlug = new Zend_Form_Element_Text('url_slug');
+        $urlSlug->addFilter('StringTrim')
+                ->addFilter(new Application_Model_Filter_UrlSlug())
+                ->addValidator(new Zend_Validate_Db_NoRecordExists(array(
+                    'table' => 'cms_sitemap_pages',
+                    'field' => 'url_slug',
+                    'exclude' => 'parent_id = ' . $this->parentID
+                )))
+                ->addValidator('StringLength', false, array('min' => 2, 'max' => 255))
+                ->setRequired(true);
+                
+        $this->addElement($urlSlug);
+        
+        $shortTitle = new Zend_Form_Element_Text('short_title');
+        $shortTitle->addFilter('StringTrim')
+                ->addValidator('StringLength', false, array('min' => 2, 'max' => 255))
+                ->setRequired(true);
+                
+        $this->addElement($shortTitle);
+        
+        $title = new Zend_Form_Element_Text('title');
+        $title->addFilter('StringTrim')
+                ->addValidator('StringLength', false, array('min' => 2, 'max' => 500))
+                ->setRequired(true);
+                
+        $this->addElement($title);
+        
+        $description = new Zend_Form_Element_Textarea('description');
+        $description->addFilter('StringTrim')
+                ->setRequired(false);
+                
+        $this->addElement($description);
+        
+        $body = new Zend_Form_Element_Textarea('body');
+        $body->setRequired(false);
+                
+        $this->addElement($body);
+    }
+
+}
+
